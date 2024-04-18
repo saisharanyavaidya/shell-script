a8a6fac73f99465eab1571cfa16f24567c813832
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-04-18 23:02:10 +0530

    functions

---
+++ functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then echo "installing $2 failed"
+    exit 1
+    else echo "installing $2 success"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then echo "not super user"
+exit 1
+else 
+echo "you are super user"
+fi
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
+dnf install git -y
+VALIDATE $? "git"
