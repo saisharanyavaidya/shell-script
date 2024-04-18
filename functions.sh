91e621f35e3e3a0f83c019315258901be10f90c2
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-04-18 23:53:06 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -5,8 +5,8 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
-W="\e[31m"
-N="\e[31m"
+W="\e[32m"
+N="\e[0m"
 
 
 VALIDATE (){
