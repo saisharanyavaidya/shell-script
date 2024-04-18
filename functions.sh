501b460b7ea75395f91892e141634d7b67d67ac9
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-04-18 23:49:56 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -4,13 +4,16 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+W="\e[31m"
+N="\e[31m"
 
 
 VALIDATE (){
     if [ $1 -ne 0 ]
-    then echo "installing $2 failed"
+    then echo "installing $2 $R failed $N ..."
     exit 1
-    else echo "installing $2 success"
+    else echo "installing $2 $G success $N ..."
     fi
 }
 
