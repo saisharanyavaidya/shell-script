5a0dd0f4a28cdfd133c2461eac1e00d493bcd8c0
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-04-18 22:08:33 +0530

    functions

---
+++ arrays.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FRUITS = ("apple" "banana" "fig" "grapes" "pears")
+
+echo "${FRUITS[0]} -- ${FRUITS[1]} -- ${FRUITS[2]} -- ${FRUITS[3]} -- ${FRUITS[4]} -- ${FRUITS[5]} -- ${FRUITS[6]}"
