75bdbe9c03862c29171f7ae2648e0d11e7077100
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-04-24 16:53:31 +0530

    concepts

---
+++ current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NAME="My name is Avyan Viraaj"
+
+echo "Name Before : $NAME"
+echo "PID Before : $$"
+
+./other-script.sh
+
+echo "Name After : $NAME"
---
+++ other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAME="My name is Avi"
+
+echo "Before Name in other : $NAME"
+echo "Before PID in other: $$"
