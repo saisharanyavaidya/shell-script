ebf7cd33b938775d839d78df4f8669ff4c1ac29a
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-04-25 17:08:25 +0530

    concepts

--- disk-usage.sh
+++ disk-usage.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
 MESSAGE=""
 
-While IFS= read -r line
+while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
