a8f96adbdc345f638e9a891067a73f847144f8e3
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-06-21 17:51:46 +0530

    changes

--- common.sh
+++ common.sh
@@ -10,11 +10,11 @@ N="\e[0m"
 
 set -e
 
-# handle_error(){
-#     echo -e "Error occured at line number: $R $1 $N, error command: $R $2 $N"
-# }
+handle_error(){
+    echo -e "Error occured at line number: $R $1 $N, error command: $R $2 $N"
+}
 
-# trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
 
 
 VALIDATE (){
